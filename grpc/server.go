c13b65a9addd8f6dedcdc233de5de790788fdb79
kumar <kumareddy14322gmail.com>
2021-01-12 14:36:48 +0530

    grpc introducton program

---
+++ grpc/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/golang/protobuf/protoc-gen-go/grpc"
+)
+
+func main() {
+	lis,err:=net.Listen("tcp",":9000")
+	if err!=nil{
+		log.Fatalf("failed to listen on port9000: %v",err)
+	}
+
+	grpcServer:=grpc.NewSever()
+
+	if err :=grpcServer.Serve(lis);
+	err !=nil{
+		log.Fatalf("failed to serer Grpc server over portt 9000: %v",err)
+	}
+}
