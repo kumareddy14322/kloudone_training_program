1902dc09747f4158fe49bc314baff094a2eeca9f
kumar <kumareddy14322gmail.com>
2021-01-15 20:09:35 +0530

    "mod example"

---
+++ mod_example/go.mod
@@ -0,0 +1,5 @@
+module github.com/kloudone_training_program/mod_example
+
+go 1.15
+
+require github.com/elliotforbes/test-package v1.0.0 // indirect
---
+++ mod_example/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	sample "github.com/elliotforbes/test-package"
+	
+)
+
+
+func  main()  {
+	fmt.Println("hi world")
+	sample.MySampleFunction()
+	
+}
